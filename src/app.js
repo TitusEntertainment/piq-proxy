128b3a1f12c04c8057e6deb450f3d75e0d9e20cb
Eddie Englund <properllerbucher@gmail.com>
2021-12-02 14:39:04 +0100

    Upgraded logging

--- src/app.js
+++ src/app.js
@@ -8,13 +8,17 @@ const port = config.port || 3001;
 
 app.use(express.json());
 
+const getCurrentTime = () => new Date().toUTCString();
+
 router.use(async (req, res) => {
     if (config.piqIpAdresses.includes(req.headers["x-forwarded-for"])) {
+        console.log(`${getCurrentTime()}: Sending data to merchant at URL ${config.merchantUrl}${req.url} with data: `, { ...req.body })
         const data = await axios.post(`${config.merchantUrl}${req.url}`, { ...req.body });
         if (data.status !== 200) {
+            console.log(`${getCurrentTime()}: Status was not 200, responding with error to PIQ`, req.url, data.data);
             return res.status(data.status).send(data.data);
         }
-        console.log(req.url, data.data);
+        console.log(`${getCurrentTime()}: Respnding to PaymentIQ with data:`, data.data);
         return res.status(200).send(data.data);
     }
     console.log(`You're doing something odd kiddo IP ${req.headers["x-forwarded"]} isn't configured in config.json`);
